332eb6425308701eb860e5a9369e96199aea136f
Magnus Burenius <magnus.burenius@gmail.com>
2024-11-16 13:22:24 +0100

    add boilerplate for plot structs and functions

--- plot/plot.c
+++ plot/plot.c
@@ -1,1 +1,24 @@
 #include "plot.h"
+
+static
+void rasterizePoints(Points points, Image image) {
+    
+}
+
+static
+void rasterizeLines(Lines lines, Image image) {
+
+}
+
+static
+void rasterizeAxes(Axes axes, Image image) {
+
+}
+
+Image rasterizePlot(Plot plot) {
+    auto image = (Image){};
+    rasterizeAxes(plot.axes, image);
+    rasterizePoints(plot.points, image);
+    rasterizeLines(plot.lines, image);
+    return image;
+}
--- plot/plot.h
+++ plot/plot.h
@@ -1,1 +1,91 @@
 #pragma once
+
+#include <stdint.h>
+
+#define auto __auto_type
+
+typedef uint32_t Color;
+
+typedef struct Point {
+    double x;
+    double y;
+    double radius;
+    Color color;
+} Point;
+
+typedef struct Line {
+    double x0;
+    double x1;
+    double y0;
+    double y1;
+    double thickness;
+    Color color;
+} Line;
+
+typedef struct VerticalLine {
+    double x;
+    double y0;
+    double y1;
+    double thickness;
+    Color color;
+} VerticalLine;
+
+typedef struct HorizontalLine {
+    double x0;
+    double x1;
+    double y;
+    double thickness;
+    Color color;
+} HorizontalLine;
+
+typedef struct Points {
+    Point* data;
+    size_t count;
+    size_t capacity;
+} Points;
+
+typedef struct Lines {
+    Line* data;
+    size_t count;
+    size_t capacity;
+} Lines;
+
+typedef struct VerticalLines {
+    VerticalLine* data;
+    size_t count;
+    size_t capacity;
+} VerticalLines;
+
+typedef struct HorizontalLines {
+    HorizontalLine* data;
+    size_t count;
+    size_t capacity;
+} HorizontalLines;
+
+typedef struct Axes {
+    double xmin;
+    double ymin;
+    double xstep;
+    double ystep;
+    double xmax;
+    double ymax;
+    const char* x_label;
+    const char* y_label;
+    const char* x_format;
+    const char* y_format;
+} Axes;
+
+typedef struct Plot {
+    Points points;
+    Lines lines;
+    Axes axes;
+} Plot;
+
+typedef struct Image {
+    Color* data;
+    size_t count;
+    size_t width;
+    size_t height;
+} Image;
+
+Image rasterizePlot(Plot plot);
