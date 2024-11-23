5e8b7c2b156b99a52a811ec38bd608e3a937efcb
Magnus Burenius <magnus.burenius@gmail.com>
2024-11-23 15:55:56 +0100

    refactor: extract xInImage

--- plot/plot.c
+++ plot/plot.c
@@ -21,17 +21,33 @@ void rasterizeLines(Lines lines, Image image) {
 }
 
 static
+size_t xInImage(double x_in_plot, Axes axes, Image image) {
+    auto xmin = axes.xmin;
+    auto xmax = axes.xmax;
+    auto width = (double)image.width;
+    return (size_t)((x_in_plot - xmin) / (xmax - xmin) * width);
+}
+
+static
+size_t yInImage(double y_in_plot, Axes axes, Image image) {
+    auto ymin = axes.ymin;
+    auto ymax = axes.ymax;
+    auto height = (double)image.height;
+    return (size_t)((y_in_plot - ymin) / (ymax - ymin) * height);
+}
+
+static
 void rasterizeAxes(Axes axes, Image image) {
     auto GRAY = makeColor(160, 160, 160, 255);
     for (double xd = axes.xmin; xd - 0.5 * axes.xstep < axes.xmax; xd += axes.xstep) {
-        auto xi = (size_t)((xd - axes.xmin) / (axes.xmax - axes.xmin) * image.width);
+        auto xi = xInImage(xd, axes, image);
         xi = xi >= image.width ? image.width - 1 : xi;
         FOR_Y(yi, image) {
             image.data[yi * image.width + xi] = GRAY;    
         }
     }
     for (double yd = axes.ymin; yd - 0.5 * axes.ystep < axes.ymax; yd += axes.ystep) {
-        auto yi = (size_t)((yd - axes.ymin) / (axes.ymax - axes.ymin) * image.height);
+        auto yi = yInImage(yd, axes, image);
         yi = yi >= image.height ? image.height - 1 : yi;
         FOR_X(xi, image) {
             image.data[yi * image.width + xi] = GRAY;
