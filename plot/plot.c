d7fcd60a37ad7192e04ff55922cddb576d97d4c5
Magnus Burenius <magnus.burenius@gmail.com>
2024-11-24 13:29:16 +0100

    tweak offset for scale of x axis

--- plot/plot.c
+++ plot/plot.c
@@ -107,7 +107,7 @@ void rasterizeAxes(Axes axes, Image image) {
 
         CLEAR(string_buffer);
         FORMAT_STRING(string_buffer, "%.1f", xd);
-        rasterizeString(string_buffer.data, xi, yi_max + 1 * 8, 1, BLACK, image);
+        rasterizeString(string_buffer.data, xi - 3 * 8 / 2, yi_max + 1 * 8, 1, BLACK, image);
     }
     // Plot scale for y axis:
     for (double yd = axes.ymin; yd - 0.5 * axes.ystep < axes.ymax; yd += axes.ystep) {
